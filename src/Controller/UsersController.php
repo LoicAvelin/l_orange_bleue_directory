00889fd0cd74ca4dfed8d25a165396d58e58c4a5
Loïc AVELIN <loic.avelin@protonmail.com>
2022-09-30 16:18:17 +0200

    Show all users on one page

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -40,4 +40,39 @@ class UsersController extends AbstractController
       "form" => $form->createView()
     ]);
   }
+
+  #[Route("/admin/edit/user/{id}")]
+  public function edit(Request $request, ManagerRegistry $doctrine, Users $user): Response
+  {
+    $form = $this->createForm(Users::class, $user);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $doctrine->getManager()->flush();
+    }
+
+    return $this->render('admin/registerUser.html.twig', [
+      "form" => $form->createView()
+    ]);
+  }
+
+  #[Route("/admin/delete/user/{id}")]
+  public function delete(ManagerRegistry $doctrine, Users $user): Response
+  {
+    $entityManager = $doctrine->getManager();
+    $entityManager->remove($user);
+    $entityManager->flush();
+
+    return $this->redirectToRoute("/admin");
+  }
+
+  #[Route("/admin/user/read-all")]
+  public function readAll(ManagerRegistry $doctrine): Response
+  {
+    $repository = $doctrine->getRepository(Users::class);
+    $users = $repository->findAll();
+    return $this->render('admin/readAllUsers.html.twig', [
+      "users" => $users
+    ]);
+  }
 }
