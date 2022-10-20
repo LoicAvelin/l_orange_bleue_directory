4f315622f56db8727405ed526928f7fd62292825
Loïc AVELIN <loic.avelin@protonmail.com>
2022-10-20 14:47:56 +0200

    update first page app and logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: "/login", name: "app_login")]
+    #[Route(path: "/", name: "app_login")]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if (
@@ -47,10 +47,11 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: "/logout", name: "app_logout")]
-    public function logout(): void
+    public function logout(): Response
     {
-        throw new \LogicException("This method can be blank - it will be intercepted by the logout key on your firewall.");
+        return $this->render("security/logout.html.twig");
     }
+
     #[Route(path: "/CGU", name: "app_CGU")]
     public function legalNotices(UsersRepository $repository): Response
     {
