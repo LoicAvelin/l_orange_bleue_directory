ed55ff5ef205d352194d5914d8eab91d09f79b2c
Loïc AVELIN <loic.avelin@protonmail.com>
2022-10-28 11:29:02 +0200

    Add keyframes to adminHomepage

--- public/assets/js/adminHome.js
+++ public/assets/js/adminHome.js
@@ -5,17 +5,27 @@ let d1 = document.getElementById("d1");
 let d2 = document.getElementById("d2");
 
 togg1.addEventListener("click", () => {
-  if(getComputedStyle(d1).display != "none"){
+  if(
+    getComputedStyle(d1).display != "none" && 
+    getComputedStyle(d1).animationPlayState != "paused"
+  ){
+    d1.style.animationPlayState = "paused";
     d1.style.display = "none";
   } else {
+    d1.style.animationPlayState = "running";
     d1.style.display = "block";
   }
 })
 
 togg2.addEventListener("click", () => {
-  if(getComputedStyle(d2).display != "none"){
+  if(
+    getComputedStyle(d2).display != "none" && 
+    getComputedStyle(d2).animationPlayState != "paused"
+  ){
     d2.style.display = "none";
+    d2.style.animationPlayState = "paused";
   } else {
     d2.style.display = "block";
+    d2.style.animationPlayState = "running";
   }
 })
